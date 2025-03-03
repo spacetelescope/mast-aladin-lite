78ecf755ee8c147e62cb53109628afc12e56413e
Tom <tdonaldson@stsci.edu>
2025-03-03 11:01:59 -0500

    Add MastAladin to top level of API (#15)

--- mast_aladin_lite/__init__.py
+++ mast_aladin_lite/__init__.py
@@ -1,1 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Top-level package for mast_aladin_lite."""
+
+# Top-level API as exposed to users.
+from .app import MastAladin  # noqa: F401
