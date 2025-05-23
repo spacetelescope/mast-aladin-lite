7691b8ae398fea275b3922dda3a8ba4c1b78fa37
Celia Parts <cparts@stsci.edu>
2025-05-23 14:36:21 -0400

    cleaning up comments

--- mast_aladin_lite/aida.py
+++ mast_aladin_lite/aida.py
@@ -4,12 +4,10 @@ class AID:
     """
     Provides API for mast-aladin-lite to allow for parity with 
     jdaviz. This is to be used as a mixin within exisiting classes.
-    This is based on the Astro Image Display API (AIDA) formerly known
-    as the astrowidgets API.
+    This is based on the Astro Image Display API (AIDA).
 
     """
 
-    # __init__ the basics
     def __init__(self, mast_aladin):
         self.app = mast_aladin
 
@@ -27,9 +25,7 @@ class AID:
             Given coordinates are not provided as SkyCoord.
 
         """
-        # add check the coords are instance of sky coords, raise warning if not
-        # common APIs method only takes center on
-
+        
         if not isinstance(point, SkyCoord):
             raise NotImplementedError
         
