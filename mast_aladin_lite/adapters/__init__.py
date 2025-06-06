a65a528ae8d006a63bfa61c7a72728ba7e65605a
Patrick Custer <pcuster@stsci.edu>
2025-06-06 09:27:25 -0400

    update __init__

--- mast_aladin_lite/adapters/__init__.py
+++ mast_aladin_lite/adapters/__init__.py
@@ -2,3 +2,4 @@ from .mast_sync_manager import MastSyncManager
 from .viewer_sync_adapter import ViewerSyncAdapter
 from .imviz_sync_adapter import ImvizSyncAdapter
 from .aladin_sync_adapter import AladinSyncAdapter
+from .viewer_sync_ui import ViewerSyncUI
