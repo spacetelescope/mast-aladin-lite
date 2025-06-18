141405b450f5a2cb69a9cfc532dc601cddf4a446
Brett M. Morris <bmmorris@stsci.edu>
2025-06-18 16:22:48 -0400

    adding method to get 'current app', renaming app fixture

--- mast_aladin_lite/app.py
+++ mast_aladin_lite/app.py
@@ -2,8 +2,26 @@ from ipyaladin import Aladin
 from mast_aladin_lite.aida import AID
 
 
+# store reference to the latest instantiation:
+_latest_instantiated_app = None
+
+
 class MastAladin(Aladin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.aid = AID(self)
+
+        global _latest_instantiated_app
+        _latest_instantiated_app = self
+
+
+def gca():
+    """
+    Get the current mast-aladin-lite application instance.
+
+    Returns
+    -------
+    MastAladin
+    """
+    return _latest_instantiated_app
--- mast_aladin_lite/tests/test_aida_example.py
+++ mast_aladin_lite/tests/test_aida_example.py
@@ -15,53 +15,53 @@ def assert_angle_close(angle1, angle2, atol=1 * u.arcsec):
     assert_quantity_allclose(difference, desired=0*u.arcsec, atol=atol)
 
 
-def test_mast_aladin_has_aid(MastAladin_helper):
-    assert hasattr(MastAladin_helper, 'aid')
-    assert callable(getattr(MastAladin_helper.aid, 'set_viewport', None))
+def test_mast_aladin_has_aid(MastAladin_app):
+    assert hasattr(MastAladin_app, 'aid')
+    assert callable(getattr(MastAladin_app.aid, 'set_viewport', None))
 
 
-def test_mast_aladin_aid_set_viewport(MastAladin_helper):
+def test_mast_aladin_aid_set_viewport(MastAladin_app):
     # check that the default center coordinate is (0, 0) deg before
     # we test the setter for center:
     default_center = SkyCoord(0, 0, unit='deg')
-    assert_coordinate_close(MastAladin_helper.target, default_center)
+    assert_coordinate_close(MastAladin_app.target, default_center)
     target_coords = SkyCoord(45, 45, unit='deg')
-    MastAladin_helper.aid.set_viewport(center=target_coords)
-    assert_coordinate_close(MastAladin_helper.target, target_coords)
+    MastAladin_app.aid.set_viewport(center=target_coords)
+    assert_coordinate_close(MastAladin_app.target, target_coords)
 
 
-def test_mast_aladin_aid_get_viewport(MastAladin_helper):
+def test_mast_aladin_aid_get_viewport(MastAladin_app):
     # check that the default center coordinate is (0, 0) deg,
     # the default fov is 60.0 deg, and the image_label is None
     default_center = SkyCoord(0, 0, unit='deg')
-    default_viewport = MastAladin_helper.aid.get_viewport()
+    default_viewport = MastAladin_app.aid.get_viewport()
     assert_coordinate_close(default_viewport["center"], default_center)
     assert_angle_close(Angle(60, u.deg), default_viewport["fov"])
     assert default_viewport["image_label"] is None
 
 
-def test_mast_aladin_aid_get_and_set_viewport_roundtrip(MastAladin_helper):
+def test_mast_aladin_aid_get_and_set_viewport_roundtrip(MastAladin_app):
     # check that performing a round trip of getting and setting the
     # viewport results in the original state
 
     # get default settings
-    default_viewport = MastAladin_helper.aid.get_viewport()
+    default_viewport = MastAladin_app.aid.get_viewport()
 
     # change viewport settings
     target_coords = SkyCoord(45, 45, unit='deg')
-    MastAladin_helper.aid.set_viewport(center=target_coords)
+    MastAladin_app.aid.set_viewport(center=target_coords)
 
     # check new viewport settings
-    midpoint_viewport = MastAladin_helper.aid.get_viewport()
+    midpoint_viewport = MastAladin_app.aid.get_viewport()
     assert_coordinate_close(midpoint_viewport["center"], target_coords)
     assert_angle_close(Angle(60, u.deg), midpoint_viewport["fov"])
     assert midpoint_viewport["image_label"] is None
 
     # change viewport settings back to default
-    MastAladin_helper.aid.set_viewport(center=default_viewport["center"])
+    MastAladin_app.aid.set_viewport(center=default_viewport["center"])
 
     # check final viewport settings
-    final_viewport = MastAladin_helper.aid.get_viewport()
+    final_viewport = MastAladin_app.aid.get_viewport()
     assert_coordinate_close(final_viewport["center"], default_viewport["center"])
     assert_angle_close(default_viewport["fov"], final_viewport["fov"])
     assert final_viewport["image_label"] is None
