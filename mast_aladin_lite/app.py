ad2fa6ac4693f70abebf0661173490eef60aa7d6
Brett M. Morris <bmmorris@stsci.edu>
2025-07-21 09:56:03 -0400

    more improvements from Tom's review

--- mast_aladin_lite/app.py
+++ mast_aladin_lite/app.py
@@ -1,7 +1,3 @@
-from astropy.coordinates import SkyCoord
-import astropy.units as u
-from astropy.utils.masked import Masked
-
 from ipyaladin import Aladin
 from mast_aladin_lite.aida import AID
 from mast_aladin_lite.table import MastTable
@@ -12,9 +8,12 @@ _latest_instantiated_app = None
 
 
 class MastAladin(Aladin):
-    selected_table = None
 
     def __init__(self, *args, **kwargs):
+        # set ICRSd as the default visible coordinate system
+        # in aladin-lite:
+        kwargs.setdefault('coo_frame', 'ICRSd')
+
         super().__init__(*args, **kwargs)
 
         # the `aid` attribute gives access to methods from the
@@ -24,9 +23,19 @@ class MastAladin(Aladin):
         global _latest_instantiated_app
         _latest_instantiated_app = self
 
-    def load_table(self, table, name='table', load_footprints=True, update_viewport=True):
-        table_widget = MastTable(table)
-        self.selected_table = table_widget
+    def load_table(
+        self,
+        table,
+        load_footprints=True,
+        update_viewport=True,
+        unique_column=None
+    ):
+        table_widget = MastTable(
+            table,
+            app=self,
+            unique_column=unique_column,
+            update_viewport=update_viewport
+        )
 
         if load_footprints:
             if 's_region' in table.colnames:
@@ -36,24 +45,6 @@ class MastAladin(Aladin):
                     "The table does not contain an `s_region` column, so no "
                     "footprints can be loaded."
                 )
-
-        if update_viewport:
-            ra_column, dec_column = 'ra', 'dec'
-
-            if ra_column not in table.colnames:
-                ra_column, dec_column = 'targ_ra', 'targ_dec'
-
-            mask = (table[ra_column] == 0) & (table[dec_column] == 0)
-
-            center_coord = SkyCoord(
-                ra=Masked(table[ra_column] * u.deg, mask).mean().unmasked,
-                dec=Masked(table[dec_column] * u.deg, mask).mean().unmasked,
-                unit=u.deg
-            )
-
-            # change the coordinate frame to match the coordinates in the MAST table:
-            self.coo_frame = 'ICRSd'
-            self.target = center_coord
 
         return table_widget
 
