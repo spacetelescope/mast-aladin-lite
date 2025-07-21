0b753958ff6a8b9d694b27b7c6b6a5ea3fcc2e82
Patrick Custer <pcuster@stsci.edu>
2025-07-21 13:21:36 -0400

    update tests

--- mast_aladin_lite/tests/adapters/test_sync_adapters.py
+++ mast_aladin_lite/tests/adapters/test_sync_adapters.py
@@ -2,6 +2,8 @@ from mast_aladin_lite.adapters import AladinSyncAdapter, ImvizSyncAdapter
 from mast_aladin_lite.app import MastAladin
 from mast_aladin_lite.tests.test_utils import BaseImviz
 
+from pytest import approx
+
 
 class TestSyncAdapters(BaseImviz):
     def test_aladin_sync_from_imviz(self):
@@ -13,16 +15,16 @@ class TestSyncAdapters(BaseImviz):
 
         # assert starting coordinate is approximately on Cartwheel Galaxy
         center = mast_aladin_sync_adapter.get_center()
-        assert center.ra.deg == 9.4213055
-        assert center.dec.deg == -33.71625419
+        assert center.ra.deg == approx(9.4213055, rel=1e-6)
+        assert center.dec.deg == approx(-33.71625419, rel=1e-7)
 
         # act - sync the mast aladin viewer to the imviz view
         mast_aladin_sync_adapter.sync_to(imviz_sync_adapter)
 
         # assert that the view has changed as expected
         center = mast_aladin_sync_adapter.get_center()
-        assert center.ra.deg == 9.425937637864708
-        assert center.dec.deg == -33.71515927986813
+        assert center.ra.deg == approx(9.425937637864708, rel=1e-14)
+        assert center.dec.deg == approx(-33.71515927986813, rel=1e-14)
 
     def test_aladin_sync_to_imviz(self):
         # arrange
@@ -34,13 +36,13 @@ class TestSyncAdapters(BaseImviz):
 
         # assert starting coordinate is approximately on Cartwheel Galaxy
         center = imviz_sync_adapter.get_center()
-        assert center.ra.deg == 9.425937637864708
-        assert center.dec.deg == -33.71515927986813
+        assert center.ra.deg == approx(9.425937637864708, rel=1e-14)
+        assert center.dec.deg == approx(-33.71515927986813, rel=1e-14)
 
         # act - sync the imviz viewer to the mast aladin viewer
         imviz_sync_adapter.sync_to(mast_aladin_sync_adapter)
 
         # assert that the view has changed as expected
         center = imviz_sync_adapter.get_center()
-        assert center.ra.deg == 9.420758834360687
-        assert center.dec.deg == -33.71643929028706
+        assert center.ra.deg == approx(9.420758834360687, rel=1e-14)
+        assert center.dec.deg == approx(-33.71643929028706, rel=1e-14)
